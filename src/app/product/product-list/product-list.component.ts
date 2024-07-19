03ab42e9ebb2aaa2e6143872fb84dcabb647381e
Romina <rominahelou@gmail.com>
2024-07-19 19:50:20 +0200

    Adding sort functionality

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit{
 
   products: Product[] = []
   filteredProducts: Product[] = []
+  sortOrder: string = ""
 
   constructor(private productServicee: ProductService, private cartService: CartService, private snackBar: MatSnackBar) {}
   
@@ -40,6 +41,19 @@ export class ProductListComponent implements OnInit{
     searchTerm = searchTerm.toLowerCase()
 
     this.filteredProducts = this.products.filter(product => product.name.toLowerCase().includes(searchTerm))
+    
+    this.sortProducts(this.sortOrder)
   }
+
+  sortProducts(sortValue: string){
+    this.sortOrder = sortValue;
+
+    if(this.sortOrder === "priceLowHigh"){
+      this.filteredProducts.sort((a,b) => a.price - b.price)
+    } else if(this.sortOrder === "priceHighLow"){
+      this.filteredProducts.sort((a,b) => b.price - a.price)
+    }
+  }
+
   
 }
