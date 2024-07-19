25305b48375b7d287c3549bab92d1d91bd4fef86
Romina <rominahelou@gmail.com>
2024-07-19 18:56:41 +0200

    Adding to cart, popup

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../../models/product';
+import { CartService } from '../../cart/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-list',
@@ -11,12 +13,24 @@ export class ProductListComponent implements OnInit{
 
   products: Product[] = []
 
-  constructor(private productServicee: ProductService) {}
+  constructor(private productServicee: ProductService, private cartService: CartService, private snackBar: MatSnackBar) {}
   
   ngOnInit(): void {
    this.productServicee.getProducts().subscribe(data => {
     this.products = data
    }) 
+  }
+
+  addToCart(product: Product): void {
+    this.cartService.addToCart(product).subscribe({
+      next: () => {
+        this.snackBar.open("Product added to cart!", "", {
+          duration: 2000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top'
+        })
+      }
+    })
   }
   
 }
